8ca2c7ed2876efcd70d4c8a0d6e2d0be205429c8
Azazel <127372621+LuanHeMuYu@users.noreply.github.com>
2025-10-19 15:41:04 +0800

    Init changes

---
+++ AutocfgWriter/Struct/Alias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutocfgWriter.Struct
+{
+    internal class Alias : Entry
+    {
+        public Alias() { }
+    }
+}
---
+++ AutocfgWriter/Struct/Entry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AutocfgWriter.Struct
+{
+    /// <summary>
+    /// This is base of Alias & Bind
+    /// </summary>
+    internal class Entry
+    {
+        /// <summary>
+        /// 每个条目的唯一标识符
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 每个条目的值 (可能后文不会用string 可能会用Entry)
+        /// </summary>
+        public string Value { get; set; }
+
+        string info() {
+            return "This is default Entry";
+        }
+    }
+}
